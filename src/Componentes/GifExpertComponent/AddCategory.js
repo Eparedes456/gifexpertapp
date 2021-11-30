e8291bf473795d03ebf1420dfcf458af4607e29d
Eparedes456 <erickparedes456@gmail.com>
2021-11-30 14:04:26 -0500

    Update AddCategory.js

--- src/Componentes/GifExpertComponent/AddCategory.js
+++ src/Componentes/GifExpertComponent/AddCategory.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 export const AddCategory = ({ setCategorias }) => {
-
+ //holi
     const [inputValue,setInputValue] = useState('');
 
     const changeValue = (e) =>{
